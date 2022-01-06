f1821245391fc71a7660f9ca614a5ef70940e1f6
Adparaguai <wid_asantos@uolinc.com>
2022-01-06 08:25:06 -0300

    refatorando o customerController

--- src/main/kotlin/br/com/apssystem/mercadolivre/controller/CustomerController.kt
+++ src/main/kotlin/br/com/apssystem/mercadolivre/controller/CustomerController.kt
@@ -8,14 +8,23 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/customers")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel{
-        return CustomerModel("1", "Adilson", "ads@yahoo.com.br")
+    fun getCustomer(): List<CustomerModel>{
+        return customers;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: CustomerModel){
-        println(customer)
+        val id = if (customers.isEmpty()){
+            1
+        }else{
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        CustomerModel(id, customer.name, customer.email)
+        println(custom)
     }
 }
